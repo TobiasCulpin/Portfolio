097a30822234898f3a028240edae69f9a358b6cc
TobiasCulpin <culpintobias@gmail.com>
2022-03-08 22:50:43 +0000

    inverted filters and all projects show when all filters are off

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ var projects = document.getElementById("projects").querySelectorAll("div")
 document.writeln()
 
 //Generating a map of bools for each filter element to keep track of which are active
-//All are on by default
-//At the same time this adds onclick events to all the filter buttons
+//All are off by default - BUT when all filters are off all the projects are shown
+//This is a similar system to comparable web design
 var filterStatus = []
 for (let i = 0; i < filters.length; i++)
 {
-    filterStatus[filters[i].classList] = true
-    filters[i].onclick = function(){ toggle(filters[i]) }
+    filterStatus[filters[i].classList] = false
+    filters[i].onclick = function(){ toggle(filters[i]) }//Adds onclick events to all the filter buttons
 }
 
 //To be called on a button click
@@ -20,28 +20,54 @@ function toggle(elem)
 {
     if (filterStatus[elem.classList])
     {
-        elem.style.background = "blue"
+        elem.style.background = "lightBlue"
         filterStatus[elem.classList] = false
     }
     else
     {
-        elem.style.background = "lightBlue"
+        elem.style.background = "blue"
         filterStatus[elem.classList] = true
     }
     updateProjects()
 }
 
+//shows all the projects
+function showAll()
+{
+    for (let i = 0; i < projects.length; i++) { projects[i].style.display = "block" }
+}
+
+//hides all the projects
+function hideAll()
+{
+    for (let i = 0; i < projects.length; i++) { projects[i].style.display = "none" }
+}
 
 
 //To be called after a filter button is pressed.  Updates which projects should be shown
 function updateProjects()
 {
+    //if show all filter is on then we show all projects and exit function
+    //showAllFilter = document.getElementById("all")
+
+    //If all filters are off all the projects are shown
+    let allFalse = true;
+    for (let i = 0; i < filters.length; i++)
+    {
+        if (filterStatus[filters[i].classList]) { allFalse = false }
+    }
+    if (allFalse)
+    {
+        showAll()
+        return
+    }
+
     //Hide the projects at the start and then show if a filter match is made
-    for (let i = 0; i < projects.length; i++) { projects[i].style.display = "none" }
+    hideAll()
 
     for (let i = 0; i < filters.length; i++)
     {
-        if (filterStatus[filters[i].classList] == true)
+        if (filterStatus[filters[i].classList])
         {
             for (let j = 0; j < projects.length; j++)
             {
